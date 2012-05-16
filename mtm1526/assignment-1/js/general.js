5d3a81dd1c2be53cb4162f7bfe101992c4611344
JoshDavis <davis.webinfo@gmail.com>
2012-05-16 15:28:15 -0400

    Creating a letter looper using JS.

---
+++ mtm1526/assignment-1/js/general.js
@@ -0,0 +1,20 @@
+//JavaScript File
+//Letter Looper
+
+var a=('');
+
+while (!a) {
+var a = prompt('Please type a letter:');
+}
+console.log(a);
+
+//if (name == '' || name == null)
+//if (name != '' || name != null)
+	for (var i=1; i< 10; i++){
+	for (var n=1; n< i; n++){
+
+	document.write(a);
+	}
+	document.write('</br>');
+}
+
