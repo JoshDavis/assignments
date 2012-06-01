2dccaf03e40f4ab0410e02b6ee65e08959c9c9de
JoshDavis <davis.webinfo@gmail.com>
2012-06-01 14:56:42 -0400

    JS and CSS is added to the mix.

---
+++ mtm1526/assignment-3/js/circle.js
@@ -0,0 +1,25 @@
+//jQuery File
+
+
+jQuery(function ($) {
+		
+		var $circle = $('#circle')
+			, $property = $('#property')
+			, $color = $('#color')
+		;
+
+	
+		$('form').on('submit', function (e) {
+			var color = $color.val();
+
+				e.preventDefault();
+	
+				if (color) {
+				$circle.css($property.val(), color);
+				}
+		});
+
+		$('#hide-show').on('click', function (e) {
+		$circle.toggle(250);
+	});
+});
