3bc1adc2e0b910ff19f363a42892e624b34b84df
JoshDavis <davis.webinfo@gmail.com>
2012-06-11 12:29:38 -0400

    Creating an Add page and also a link within our index.

--- dinosaurs/index.php
+++ dinosaurs/index.php
@@ -20,6 +20,9 @@ $results = $sql->fetchAll();
 </head>
 
 <body>
+
+	<a href="add.php">Add a Dinosaur</a>
+
 		<?php foreach ($results as $dino) : ?>
 		<h2><a href="single.php?id=<?php echo $dino['id'];?>">
 		<?php echo $dino['dino_name']; ?></a></h2>
