b2f9c0b8a4966ff0c1bd34e6fac3cad495752816
JoshDavis <davis.webinfo@gmail.com>
2012-06-11 12:41:32 -0400

    Adding the HTML for the add page.

--- dinosaurs/add.php
+++ dinosaurs/add.php
@@ -1,10 +1,43 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
+    <meta charset="utf-8">
+    <title>Add a Dinosaur</title>
 </head>
 
 <body>
+	
+   	<h1>Add New Dinosaur</h1>
+		
+        <form method="post" action="add">
+    
+    	<div>
+        	<label for="dino_name">Dinosaur Name</label>
+            <input id="dino_name" name="dino_name" required>
+        </div>
+    	
+        <fieldset>
+        	<legend>Relationship with meat</legend>
+        	<input type="radio" id="love" name="loves_meat" value="1">
+            <label for="love">Loves Meat</label>
+            <input type="radio" id="hate" name="loves_meat" value="0">
+            <label for="hate">Hates Meat</label>
+        </fieldset>
+        
+        <div>
+        	<input type="checkbox" id="in_jurassic_park" name="in_jurassic_park">
+            <label for="in_jurassic_park">In Jurassic Park</label>
+        </div>
+        
+        <button type="submit">Add</button>
+    
+    
+    
+    </form>
+
+
+
+
+
 </body>
 </html>
