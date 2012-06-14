ce01bbc64209bf51dfb46092dc298839322aba5f
JoshDavis <davis.webinfo@gmail.com>
2012-06-14 08:59:44 -0400

    Adding index page and creating connection with movie database.

---
+++ mtm1531/assignment-5/single.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once 'includes/db.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+
+//Prepare allows us to have variables in out SQL
+//We can create placeholders and later fill them with some content.
+//Prepare is used to protect against SQL injection attacks.
+$sql = $db->prepare('
+SELECT id, movie_title, director, release_date
+FROM movies
+WHERE id = :id
+');
+
+$sql ->bindValue(':id', $id, PDO::PARAM_INT);
+$sql -> execute();
+$results = $sql->fetch();
+
+?>
+
+
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $results ['movie_title'];?>&middot; Movie Date Base</title>
+    <link href="css/general.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<h1><?php echo $results['movie_title']; ?></h1>
+    <dl>
+    	    <dt>Director</dt>
+            <dd><?php echo $results['director'];?></dd>
+            
+            <dt>In Jurassic Park</dt>
+            <dd><?php echo $results['release_date'];?></dd>
+    </dl>
+    
+    <a href="delete.php?id=<?php echo $id; ?>">Delete Movie</a>
+    <a href="index.php">Go back to Movie Database</a>
+
+	
+
+
+</body>
+</html>
